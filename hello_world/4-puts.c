84cf69674c2885aa8ddc77dfe1f563789e2716aa
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-17 16:44:12 +0000

    use puts to print phrase followed by a new line and ends with 0

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- * main - is the entry point
- *
- * Return (0) successful
-*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+/*
+ * sum - print "Programming is like building a multilingual puzzle
+ * @arg - no arguments
+*/
