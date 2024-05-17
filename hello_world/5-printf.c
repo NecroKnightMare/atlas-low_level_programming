0ccbf352baf6ea5316d13ba7c94323502f4ed601
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-17 18:42:21 +0000

    use printf to print string with return of 0

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - void
+ *@: no parameters
+ *
+ *Description: use printf to print string
+ *Return: Always success (0)
+*/
+
+int main(void)
+}
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
