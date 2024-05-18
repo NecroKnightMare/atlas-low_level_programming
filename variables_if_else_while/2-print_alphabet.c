cbc2bc700ef02045367ad8ef40f122f62466dcaa
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-18 21:42:17 +0000

    print lowercase alpha with only putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - void
+ * @: no parameters
+ *
+ * Description: print the alphabet a-z in lowercase followed by a newline
+ * Return: Always success (0)
+*/
+int main(void)
+	putchar letter;
+
+	while (letter = 'a'; letter ,= 'z'; letter++)
+{
+	putchar("letter");
+	return (0);
+}
