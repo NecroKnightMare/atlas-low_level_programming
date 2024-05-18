0ff326ee5fccb5f00315dd785c0831a3e41597e9
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-18 23:18:47 +0000

    print single digit numbers followed by newline only using printchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - void
+ * @: no parameters
+ *
+ * Description: print single digits followed by new line; cant use char; use putchar twice
+ * Return: Always success (0)
+**/
+int main(void)
+{
+	int i;
+	
+	while(i <= 9);
+{
+	putchar(i);
+	i++;
+}
+	putchar("\n");
+	return (0);
+}
