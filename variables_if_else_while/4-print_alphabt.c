8a2215c34c0c7ab471c3ae9aa908147809c0f8a7
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-18 22:30:10 +0000

    print lowercase ignoring e and q and newline

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - void
+ * @: no parameters
+ *
+ * Description: ignore q and e
+ * Return: Always success (0)
+**/
+int main(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+}
+	putchar(i);
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
+grep -i [eq]
