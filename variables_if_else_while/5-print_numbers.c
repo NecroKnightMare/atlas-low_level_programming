29d0dc20c57e3a607f603da9925892ea842cf3ce
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-18 22:58:32 +0000

    print single digit numbers followed by newline

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - void
+ * @: no parameters
+ *
+ * Description: print all single digit numbers of base 10 from 0 and neewline
+ * Return: Always success (0)
+**/
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+{
+		printf("%d ", i);
+}
+	printf("\n");
+	return (0);
+}
