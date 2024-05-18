77c680e56be5164ca12a129d9c330fdba0192f2b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-18 23:33:53 +0000

    print alpha backwards and newline

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - void
+ * @: no parameters
+ *
+ * Description: prints alpha in lowercase and reverse
+ * Return: Always success (0)
+**/
+int main(void)
+{
+	int i = 'z';
+
+	while(i >= 'a')
+{
+	putchar(i);
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
