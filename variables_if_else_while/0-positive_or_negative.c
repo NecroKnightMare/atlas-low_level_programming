1d213219b127a296263427e6776f4fa5ebe4fd5d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-18 21:17:36 +0000

    use if else while loop to print if numer is greater, equal, or less than 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - void
+ * @: no parameters
+ *
+ * Return: Always Success (0)
+*/
+
+int main(void)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if ($n > 0)
+		printf ("is positive\n")
+	else ($n == 0);
+		printf ("is zero\n")
+	while ($n < 0);
+		printf ("is negative\n")
+	return (0);
+}
