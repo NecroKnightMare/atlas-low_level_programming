96b9241d87fdd5e5f3ca4615bf765d16d096722e
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-19 02:59:08 +0000

    print hexadecimal characters 0-9 and a-f using 3 putchars

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,9 +10,13 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 15; n++)
+	for (n = 0; n <= 9; n++)
 {
-	putchar(n + 0);
+		putchar(n);
+}
+	for (n = 'a'; n <= 'f'; n++)
+{
+		putchar(n);
 }
 	putchar('\n');
 	return (0);
