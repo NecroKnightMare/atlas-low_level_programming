dc233761b0f11dc092e777d6878c86889bd9e652
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-19 00:24:57 +0000

    print single digits from ascending order no char types only 4 putchars

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - void
+ * @: no parameters
+ *
+ * Description: print all possible combos of single digit numbers in ascending order; no char
+ * Return: Always success (0)
+**/
+int main(void)
+{
+	int
