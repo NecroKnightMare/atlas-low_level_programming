e43bd75fbdeac33421785393cc7f49f8519e6bee
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-20 14:50:47 +0000

    print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - void
+ * @: no parameters
+ *
+ * Description: print _putchar
+ * Return: Always success (0)
+**/
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
