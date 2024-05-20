f4c9904e768ae2ba63024d41f425a335b161e8ec
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-20 18:09:35 +0000

    print lower alpha

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _putchar - prints string
+ * @c: char
+ *
+ * Description: print alpha in lower case
+ * Return: Always success (0)
+**/
+
+int _putchar(char c);
+{
+    char c = 'a';
+
+    while (c <= 'z')
+{
+        _putchar(c);
+        c++;
+}
+    _putchar('\n');
+    return (0)
+}
