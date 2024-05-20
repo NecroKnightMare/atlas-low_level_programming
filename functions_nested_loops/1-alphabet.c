28b3695eb97c5bc320e7cac8087ff453daefd1b6
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-20 15:48:48 +0000

    print lower alpha

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,8 +4,18 @@
  * main - void
  * @: no parameters
  *
- * Description: print alpha in lower with new line
+ * Description: print alpha in lower case
  * Return: Always success (0)
 **/
 int main(void)
 {
+	int = 'a'
+	
+	while (i <= 'z')
+{
+		_putchar(i);
+		i++;
+}
+	_putchar('\n');
+	return (0);
+}
