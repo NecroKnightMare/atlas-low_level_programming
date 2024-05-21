60e76ee82da6a446599a33fccbf0f43e45105200
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-21 02:11:26 +0000

    absolute value

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
 /**
- * main - void
+ * _abs - absolute value
  * @: no parameters
  *
  * Description: print alpha in lower with new line
  * Return: Always success (0)
 **/
-int main(void)
+int _abs(int)
 {
+	if (n >= 0)
+{
+		return (n);
+}
+	else
+{
+		return (-n);
+}
+}
