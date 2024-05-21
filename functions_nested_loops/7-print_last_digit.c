19b4b251856c1e7ee34d3abeb5bf13f117d373c4
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-21 04:51:53 +0000

    print last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
 /**
- * main - void
+ * print_last_digit - void
  * @: no parameters
  *
  * Description: print alpha in lower with new line
  * Return: Always success (0)
 **/
-int main(void)
+int print_last_digit(int)
 {
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+{
+		last_digit *= -1;
+}
+	_putchar(last_digit + '0');
+	return (last_digit;
+}
