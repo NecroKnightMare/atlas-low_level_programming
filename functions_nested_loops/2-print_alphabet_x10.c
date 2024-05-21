645a447f9eda76e9286155d4999d1da491b3c3fd
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-21 01:30:12 +0000

    print lower alpha ten times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,14 @@
 
 void print_alphabet_x10(void)
 {
-	int i = 0
-	char letter;
+	int i = 0;
+	char l = 'a';
 
-	while (i++c <= 9)
+	while (i++ <= 9)
 {
-		for (letter = 'a'; letter <= 'z'; ++letter)
+		for (l = 'a'; l <= 'z'; ++l)
 {
-			_putchar(letter);
+			_putchar(l);
 }
 		_putchar('\n');
 }
