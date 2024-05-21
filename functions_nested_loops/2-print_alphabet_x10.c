f4e00e48014e218ed404c86e7dba73eec5b67702
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-21 01:24:49 +0000

    print lower alpha ten times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,8 +4,19 @@
  * main - void
  * @: no parameters
  *
- * Description: print alpha in lower with new line
+ * Description: print alpha ten times
  * Return: Always success (0)
 **/
-int main(void)
+
+void print_alphabet_x10(void)
 {
+	char letter;
+	for (int i = 0; i < 10; ++i)
+{
+		for (letter = 'a'; letter <= 'z'; ++letter)
+{
+			_putchar(letter);
+}
+		_putchar('\n');
+}
+}
