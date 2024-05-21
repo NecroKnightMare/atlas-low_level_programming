f396cc597364b28bc4a7fb88e460b3972afb757e
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-21 01:12:50 +0000

    print lower alpha

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -10,12 +10,12 @@
 
 void print_alphabet(void)
 {
-    int i = 97;
+	int i = 97;
 
-    while (i <= 122)
+	while (i <= 122)
 {
         _putchar(i);
         i++;
 }
-    _putchar('\n');
+    	_putchar('\n');
 }
