86943e5856f4cd8994fee8ec63b9083e0eae4eb4
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-22 15:07:31 +0000

    return 1 if c is upper and return 0 if c is lower

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isupper - ret one if c is upper
+ * @c: character parameter
+ * Return: 1 if c and 0 if success
+*/
+
+int _isupper(int c)
+{
+	char c = 'C';
+
+	if (isupper(c))
+{
+		return 1;
+}
+	else (islower(c));
+{
+		return 0;
+}
+}
