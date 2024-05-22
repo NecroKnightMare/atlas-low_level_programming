4f11e876c8dbd448ae3c85e901230808eb4f423e
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-22 01:08:32 +0000

    print random n along with the last digit; check if last digit is less than 5, greater than 6 and not zero, or equal to zero

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,12 +16,19 @@ int main(void)
         srand(time(0));
         n = rand() - RAND_MAX / 2;
         l = n % 10;
+
         if (l > 5)
+{
                 printf("Last digit of %d is %d and is greater than 5", n, l);
-        else if (l < 6 && l != 0)
-                printf("Last digit of %d is %d and is less than 6 and not zero", n, l);
-        else  (l = 0);
-                printf("Last digit of %d is %d and is 0", n, l);
+} 
+	 else if (l < 6 && l != 0)
+{
+		printf("Last digit of %d is %d and is less than 6 and not zero", n, l);
+}
+	else  (l = 0);
+{
+		printf("Last digit of %d is %d and is 0", n, l);
+}
 	putchar('\n');
         return (0);
 }
