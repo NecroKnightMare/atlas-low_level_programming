0a706575fe62e595e4f8ab9d60da176073122d93
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-26 22:53:57 +0000

    range for upper c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isupper - ret one if c is upper
@@ -9,12 +8,8 @@
 
 int _isupper(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	for (i = 32; i <= 126; i++)
 	{
-		return 1;
 	}
-	else
-	{
-		return 0;
-	}
+	return 0;
 }
