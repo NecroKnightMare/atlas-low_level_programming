ea94ee623e18cb9e32c7a83018484e25a1dcf8d3
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-27 22:33:32 +0000

    checking if c is 0-9

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,18 @@
-#include <main.h>
+#include "main.h"
+
 /**
- *  - 
- * @: 
- * Returns: 
+ * _isdigit - 0-9 
+ * @c: integer
+ * Returns: 1 if digit o otherwise
+*/
+
+int _isdigit(int c)
+{
+        for (c = 0; c <= 9; c++)
+{
+	return 1;
+                if (c != 0) && (c !<= 9)
+{
+                        return 0;
+}
+}
--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,5 +1,19 @@
-#include <main.h>
+#include "main.h"
+
 /**
- *  - 
- * @: 
- * Returns: 
+ * _isdigit - 0-9 
+ * @c: integer
+ * Returns: 1 if digit o otherwise
+*/
+
+int _isdigit(int c)
+{
+	int c;
+	for (c = 0; c <= 9; c++)
+{
+		if (c >=65)
+{
+			return 0;
+}
+}
+}
