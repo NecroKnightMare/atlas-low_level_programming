81e6bb7cc879bc09c2908b6c5acf58b517647313
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-28 01:06:57 +0000

    print 0-14 ten times with new line

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,25 @@
-#include <main.h>
+#include "main.h"
+
 /**
- *  - 
- * @: 
- * Returns: 
+ * more_numbers - repeat 0-14 10 times
+ * @: no parameters
+ * Return: 1 void
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+	for (i = 0; i <=14; i++)
+{
+		for (j = 0; j <= 10; j++)
+{
+			if (j > 10)
+{
+				_putchar((j / 10) + '0');
+}
+			_putchar((j % 10) + '0');
+}
+		_putchar('\n');
+}
+}
