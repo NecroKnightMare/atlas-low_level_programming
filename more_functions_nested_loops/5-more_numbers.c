361d8706953739b851745d59f5a97da8adf74cf2
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-28 01:09:02 +0000

    print 0-14 ten times with new line

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	for (i = 0; i <=14; i++)
+	for (i = 0; i <=10; i++)
 {
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j <= 13; j++)
 {
-			if (j > 10)
+			if (j > 9)
 {
 				_putchar((j / 10) + '0');
 }
