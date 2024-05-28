d374df22a6d4e131e88ae0f80ec2f55303882d03
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-28 03:59:51 +0000

    print string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse 
+ * @s: string
+ * Return: 1 void
+*/
+
+void print_rev(char *s)
+{
+	if (*s)
+{
+		print_rev(s + 1);
+		_putchar(*s);
+}
+}
