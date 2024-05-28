93befbe84cab37e845bdefd9a03ad1b0b0f3e195
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-28 02:00:28 +0000

    print # to make a square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,26 @@
-#include <main.h>
+#include "main.h"
+
 /**
- *  - 
- * @: 
- * Returns: 
+ * print_square - print square with #
+ * @size: size of square
+ * Return: 1 void
+*/
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+{
+		_putchar('\n');
+}
+		for (i = 0; i < size; i++)
+{
+			for (j = 0; j < size; j++)
+{
+				_putchar('#');
+}
+			_putchar('\n');
+}
+}
