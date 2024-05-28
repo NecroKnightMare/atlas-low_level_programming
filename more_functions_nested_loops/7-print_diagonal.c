b630a1127942f605ebddac5faa9ccf560d28224c
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-28 01:50:13 +0000

    print \ for numbers greater than 1

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,15 @@ void print_diagonal(int n)
         else
 {
 		int i;
+		int j;
 
 		for (i = 0; i < n; i++)
 {
                         _putchar('\\');
 }
-                _putchar('\n');
+			for (j = 0; j == i; j++)
+{
+ 		               _putchar(' ');
+}
 }
 }
