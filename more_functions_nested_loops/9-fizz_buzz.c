6b63fe8855ec8e6f4e024492dba48a3dafdbfee7
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-28 02:15:39 +0000

    print FizzBuzz with tab

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,31 @@
-#include <main.h>
+#include "main.h"
+#include <stdio.h>
 /**
- *  - 
- * @: 
- * Returns: 
+ * main - void
+ * @: no parameters
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i =1; i <= 100; i++)
+{
+		if ((i % 3 == 0) && (i % 5 == 0))
+{
+			printf("FizzBuzz\t");
+}
+		else if (i % 3 == 0)
+{
+			printf("Fizz\t");
+}
+		else if (i % 5 == 0)
+{
+			printf("Buzz\t");
+}
+		else
+			_putchar('\t');
+}
+	return (0);
+}
