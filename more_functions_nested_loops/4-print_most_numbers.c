77da1e73151fc9b7b1b47e7b42cf5a735843a66b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-28 00:50:06 +0000

    print 0-9 with newline skipping 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_most_numbers - 0-9 not 2 or 4 
+ * print_most_numbers - 0-9 not 2 or 4
  * @: no parameters
  * Return: 1 void
 */
@@ -10,7 +10,7 @@
 void print_most_numbers(void)
 {
 	int c;
-	for (c = 0; c <=9; c++)
+	for (c = 0; c <= 9; c++)
 {
 		if (c == 2 || c == 4)
 {
