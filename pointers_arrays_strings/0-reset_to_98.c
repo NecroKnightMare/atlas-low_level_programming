09308c66b8c484d24a0d461644305f5fcf5dbd23
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-05-28 03:19:46 +0000

    change value of n to the value of pointer n

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - reset value to pointer value 
+ * reset_to_98 - reset value to pointer value
  * @n: pointer will be new value
  * Return: 1 void
 */
 
 void reset_to_98(int *n)
 {
-        *n = 98;
+	*n = 98;
 }
