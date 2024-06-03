bc9a0c04de36a1cd4a997e2a28112d38babe26df
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-03 00:01:38 +0000

    concatonate or append two strings to return and use n bytes from string 2

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * *_strncat - concatonate two strings
  * @dest: string 1
@@ -7,21 +8,21 @@
  * Return: dest
 **/
 char *_strncat(char *dest, char *src, int n)
-{	
+{
 	char *result = dest;
-        
-        while (*dest != '\0')
-{               
-                dest++;
-}               
-        while (*src != '\0' && n > 0)
-{       
-                *dest = *src;
-                dest++;
-                src++;
+
+	while (*dest != '\0')
+{
+		dest++;
+}
+	while (*src != '\0' && n > 0)
+{
+		*dest = *src;
+		dest++;
+		src++;
 		n--;
 }
         *dest = '\0';
-        
-        return (result);
-}      
+
+	return (result);
+}
