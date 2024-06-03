a42b8dd9976804d19729cc8ebefbf3e81e60af54
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-03 04:38:48 +0000

    updating header files - 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: substring pointer or NULL
+**/
+char *_strstr(char *haystack, char *needle)
+{
