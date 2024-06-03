28bd287e72c117fed8ea25fe76b60dcdd7b63d36
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-03 21:55:19 +0000

    locates a substring 'needle in a haystack'

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -8,3 +8,14 @@
 **/
 char *_strstr(char *haystack, char *needle)
 {
+	int i = 0;
+
+	while (haystack[i] != '\0')
+{
+		haystack++;
+		for (;haystack[i] == needle[i]; i++)
+			if (!(needle[i]))
+				return (haystack);
+}
+	return (0);
+}
