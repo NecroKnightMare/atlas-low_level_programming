f6d170fe408dba3e11fc8d4f467ec3969fdf0011
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-03 04:29:19 +0000

    updating header files - 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copy a memory area
+ * @dest: destination
+ * @src: memory area
+ * @n: bytes
+ * Return: dest
+**/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
