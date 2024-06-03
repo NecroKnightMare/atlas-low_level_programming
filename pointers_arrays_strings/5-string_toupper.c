549683391d41ba828972a43963abbef69d72f413
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-03 02:58:22 +0000

    reverses an array of integers

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *string_toupper - upper case string
+ * @*: pointer
+ * Return: 0
+**/
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+{
+		if (i >= 'a' && i <= 'z')
+			i -= 32;
+}
+	return (0);
+}
