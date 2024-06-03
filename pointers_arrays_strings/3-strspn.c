a8b061b6e2eac6e63902e0be09f1199b848aca49
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-03 04:34:15 +0000

    updating header files - 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strspn - gets length of a prefix substring
+ * @s: string length
+ * @accept: bytes
+ * Return: number of bytes
+**/
+unsigned int _strspn(char *s, char *accept)
+{
