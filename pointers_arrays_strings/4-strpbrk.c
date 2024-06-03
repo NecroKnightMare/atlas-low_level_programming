ee0a23400cd59fdac316489068c85d5f3bd85e99
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-03 04:36:41 +0000

    updating header files - 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - searches string for set of bytes
+ * @s: string
+ * @accept: string
+ * Return: byte pointer or NULL
+**/
+char *_strpbrk(char *s, char *accept)
+{
