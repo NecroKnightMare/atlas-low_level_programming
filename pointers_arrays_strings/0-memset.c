5a4aaa756ebd5a33b63765117c025b43412378f2
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-03 04:26:51 +0000

    updating header files - 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_memset - fill memory with constant byte
+ * @s: pointer
+ * @b: byte
+ * @n: memory
+ * Return: a pointer
+**/
+char *_memset(char *s, char b, unsigned int n)
+{
