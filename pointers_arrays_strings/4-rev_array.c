e22439e244c60ace770d0ea066c2eef4dab42845
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-03 02:38:22 +0000

    reverses an array of integers

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of integers
+ * @a: array
+ * @n: variable
+ * Return: void 1
+**/
+void reverse_array(int *a, int n)
+{	
+	int temp;
+
+	while (n > *a)
+{
+		temp = n;
+		n = *a;
+		*a = temp;
+		a++;
+		n--;
+}
+}
