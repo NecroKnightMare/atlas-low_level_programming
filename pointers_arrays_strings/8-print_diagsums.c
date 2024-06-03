f1cbe0b04634eeef04d298ff6e1ec6cee6a11d6a
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-03 22:48:19 +0000

    print sum of two diagonals

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - print 2 diagonals of a square matrix of int
@@ -6,5 +7,17 @@
  * @size: rows
  * Return: 0
 **/
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
+	int sum1 = 0;
+	int sum2 = 0;
+	int i = 0;
+
+	while (i < size)
+{
+		i++;
+		sum1 += a[(size + 1) * i]; 
+		sum2 += a[(size - 1) * (i + 1)];
+}
+		printf("%d\n%d\n", sum1, sum2);
+}
