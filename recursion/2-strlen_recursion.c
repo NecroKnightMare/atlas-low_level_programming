ac2244ea6f66b28a9a855cab07357d2626c508f5
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-10 13:22:20 +0000

    files 0-4 have content

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,5 +6,9 @@
 **/
 int _strlen_recursion(char *s)
 {
-	return(0);
+	if (*s != '\0')
+{
+		return 1 + _strlen_recursion(s + 1);
+}
+	return (0);
 }
--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -6,5 +6,13 @@
 **/
 int factorial(int n)
 {
-	return(0);
+	if (n < 0)
+{
+		return (-1);
+}
+	else
+{
+		return (n * factorial(n - 1);
+}
+	return (0);
 }
