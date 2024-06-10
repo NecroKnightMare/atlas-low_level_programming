648abbf1470f40aa8971ee3a90c487d771bb530e
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-10 00:17:38 +0000

    file structure

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - find natural square root
+ * @n: variable
+ * Return: -1 if variable isn't natural square root
+**/
+int _sqrt_recursion(int n)
+{
+	return(-1);
+}
