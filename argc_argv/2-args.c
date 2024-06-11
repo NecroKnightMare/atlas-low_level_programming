ea2c6ac8cb5122295540272af2e783f882e97995
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-11 04:01:36 +0000

    print array argv

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,1 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ *main - size of argc
+ *@argc: array size
+ *@argv: array with arguments
+ *Return: 0
+**/
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	while (i < argc)
+{
+		printf("%s\n", argv[i]);
+		i++;
+}
+	return (0);
+}
