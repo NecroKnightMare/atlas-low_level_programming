d932f1145280314709a85baa0936b2ccbdee7465
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-11 04:33:41 +0000

    multipy two numbers argv

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,1 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply two numbers
+ * @argc: 
+ * @argv: 
+ * Return: 1
+**/
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+{
+		printf("Error\n");
+		return (1);
+}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
