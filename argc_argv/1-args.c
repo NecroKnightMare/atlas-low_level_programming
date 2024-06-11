ed9723c8930ab382af98f6acffcd93d52dabc78d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-11 03:42:19 +0000

    print int for file value, argv passing as itself to pass no errors-using void to bypass bar foo

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *main - 
+ *main - print value of file
  *@argc:
  *@argv:
 **/
 int main(int argc, char* argv[])
 {
-	argv = argv;	
+	(void)argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
