068329e4412106ff29fb7ba624bf527499ae204a
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-11 03:23:01 +0000

    print numbers passed  with argv

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,1 +1,18 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ *main - 
+ *@argc:
+ *@argv:
+**/
+int main(int argc, char* argv[])
+{
+	int i = 0;
+
+	while (i < argv)
+{		
+		i++;
+		printf("%d\n", argc - 1);
+}
+	return (0);
+}
