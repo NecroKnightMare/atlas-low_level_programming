e870c664bff5d52ecff7db405eae34978eedc2f6
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-11 02:57:31 +0000

    rename file with argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,1 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * main - renames file
+ * @argc:
+ * @argv:
+ * Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	printf("%s\n", *argv);
+	return (0);
+}
