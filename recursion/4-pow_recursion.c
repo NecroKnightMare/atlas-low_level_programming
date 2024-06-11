fff3b4a2daea605e7499e7c1ab103435bb3a3a7d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-11 00:37:50 +0000

    print power of recursively

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -11,10 +11,13 @@ int _pow_recursion(int x, int y)
 {
 		return (-1);
 }
-
+	if (y == 0)
+{
+		return (1);
+}
 	else
 {
-		return (x * (x - 1));
+		return (x *_pow_recursion (x, y - 1));
 }
 	return (0);
 }
