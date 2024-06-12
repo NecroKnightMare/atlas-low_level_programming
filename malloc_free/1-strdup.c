0e986b0ed38442fad1f196beac22cd9f609b1ad6
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-12 19:13:29 +0000

    unfinished, not compiled file 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicate string in malloc
+ * @str: string
+ * Return: NULL if 0 or fails, str if success
+**/
+char *_strdup(char *str)
+{
+	int i = 0;
+	int size = 0; 
+	size = malloc((*str + _strdup) + 1);
+	if (str == NULL)
+{
+		return (NULL);
+}  
+	if (str == 0)
+{
+		return (NULL);
+}
+	if (str = _strdup) != NULL
+{
+		return (str);
+}
+	return (0);
+}
