c8e0d413097deb6b66b8ba7c65a774f420b73034
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-12 15:40:05 +0000

    first file learned in Pods

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,2 +1,32 @@
 #include "main.h"
 
+/**
+ * create_array - create array and initialize with spec char
+ * @size: size of array
+ * @c: charcter
+ * Return: NULL or to the array
+**/
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+	char *str_array = NULL;
+	
+	str_array = malloc(sizeof(char) * size) + 1;
+
+	if (str_array == NULL)
+{
+		return (NULL);
+}
+	if (size == 0)
+{
+		return (NULL);
+}
+	while (i < size)
+{
+		i++;
+		str_array[i] = c
+
+		free (str_array);
+}
+	return (str_array);
+}
