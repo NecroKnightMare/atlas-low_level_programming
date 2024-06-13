9c1d466b2e64e1c20d2662b9dbf7c35cb5619ec3
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-13 21:24:51 +0000

    2 file base structure

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenate 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: NULL if fail, empty if success
+**/
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+
+}
