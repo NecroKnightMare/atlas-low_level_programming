87296f0dad0dbd589abcfd44c66f815725b2f5ed
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-13 19:55:58 +0000

    redoing 0 file

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,14 +10,14 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int  i = 0;
 	char *str_array = NULL;
-	
-	str_array = malloc(size);
 
-	if (str_array == NULL)
+	if (size == 0)
 {
 		return (NULL);
 }
-	if (size == 0)
+	str_array = malloc(size);
+
+	if (str_array == NULL)
 {
 		return (NULL);
 }
