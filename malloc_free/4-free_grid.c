c20509763b15ea0d2fc0fcbf4fa81063880fc528
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-13 21:33:56 +0000

    4 file base structure

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - free grid in file 3
+ * @grid: grid pointer
+ * @height: h of grid
+ * Return: NULL
+**/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+}
