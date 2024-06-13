8b48c54055c53de20304c8e0d6ce57e6b4a9162f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-13 21:28:36 +0000

    3 file base structure

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - allocate 2d array
+ * @width: w of grid
+ * @height: h of grid
+ * Return: NULL failure or w or h is 0 NULL, 0 if success
+**/
+int **alloc_grid(int width, int height)
+{
+	int i;
+
+}
