1798a42b3837a5f3af9c35887e7f43470cbf5f8e
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-16 23:59:44 +0000

    use argv and argc to read file name

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - title of file
+ * @argv: reads argc
+ * @argc: reads argv
+ * Returns: file name
+**/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0];
+		return (0);
+}
