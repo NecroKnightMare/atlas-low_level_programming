e8b5458878c5410948abd53d808d3d3bcdb7618d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-16 16:45:31 +0000

    going to push to get points in the very least. - missing duplication

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -8,21 +8,19 @@
 char *_strdup(char *str)
 {
 	int i;
-	int size;
+	char s;
 
 	if (str == NULL)
 {
 		return(NULL);
 }
-        for (i = 0; i < size; i++)
-{
-                size++;
-}
-	str = malloc(sizeof(char*) * size + 1);
+	s = malloc(sizeof(char*) * strlen + 1);
 
 	if (str == 0)
 {
 		return (NULL);
 }
-		return (str);
+	_strdup = (s, str);
+	
+	return (str);
 }
