cdcf2bca078dd8d975146a003cf3fb9b27b7b642
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-16 18:33:37 +0000

    file 2 header

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocate memory for array
+ * @nmemb: array
+ * @size: size of array
+ * Return: if 0 or fails, NULL
+**/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
