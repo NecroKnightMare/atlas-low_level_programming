865126b32e553e538c5980a2ae9baabb9e51fbef
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-16 18:30:36 +0000

    file 1 header

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenate 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes
+ * Return: if empty or fails, NULL; pointer if success
+**/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
