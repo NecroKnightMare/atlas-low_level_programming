bffc1a50eac9181514b992d7f94fbb98cfdbc8a8
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-16 22:34:05 +0000

    concatenate 2 strings, cant seem to figure these out, doesn't look/feel right

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * string_nconcat - concatenate 2 strings
  * @s1: string 1
@@ -9,5 +10,32 @@
 **/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int len = 0;
+	char *s;
 
+	if (s1 == NULL && s2 == NULL)
+{
+		return(NULL);
+}
+	if (s1[i])
+{
+		i++;
+}
+	if (s2[j])
+{
+		j++;
+}
+	if (n <= j)
+{
+		len = i + j;
+}
+	s = malloc(sizeof(char) * len + 1);
+	
+	if (s == NULL)
+{
+		return(NULL);
+}
+	return(s);
 }
