953a06f59b13389677b793c7343bf6ee53d36edb
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-16 23:18:32 +0000

    find range of array, did this in live coding, switched it up and used own variables, didn't know how else to do it, will look into other ways

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -8,5 +8,25 @@
 **/
 int *array_range(int min, int max)
 {
+	int *int_aa = 0;
+	int t_s = 0;
+	int i;
 
+	if (min > max)
+{
+		return(NULL);
+}
+	t_s = ((max-min) +1);
+
+	int_aa = malloc(sizeof(int) * t_s);
+
+	if (int_aa == NULL)
+{
+		return(NULL);
+}
+	for (i = 0; i < t_s; i++)
+{
+		int_aa[i] = min + i;
+}
+	return(int_aa);
 }
