67caf2a7069dae337cd98e63c67165bb533eb049
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-16 18:36:52 +0000

    file 3 header- live code PLD

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - min and max
+ * @min: minimum size of array
+ * @max: maximum size of array
+ * Return: min > max or fails, NULL; pointer to the new array
+**/
+int *array_range(int min, int max)
+{
+
+}
