efdaf363efa4897edb49a5247c50cac69141407f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-17 23:16:57 +0000

    retrying to concatenate 2 strings- file 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,27 +9,26 @@
 **/
 char *str_concat(char *s1, char *s2)
 {
-        int i = 0;
-        int j = 0;
-        char *s3;
+	int i = 0;
+	int j = 0;
+	char *s3;
 
-        if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 {
-                return (" ");
+		return (" ");
 }
-        s3 = malloc((sizeof(s1) + sizeof(s2)) * sizeof(char) + 1);
-
-        while (s1[i] != '\0')
+	s3 = malloc((sizeof(s1) + sizeof(s2)) * sizeof(char) + 1);
+	while (s1[i] != '\0')
 {
-                s3[j] = s1[i];
-                i++;
-                j++;
+		s3[j] = s1[i];
+		i++;
+		j++;
 }
-        while (s2[i] != '\0')
+	while (s2[i] != '\0')
 {
-                s3[j] = s2[i];
-                i++;
-                j++;
+		s3[j] = s2[i];
+		i++;
+		j++;
 }
-        return (s3);
+	return (s3);
 }
