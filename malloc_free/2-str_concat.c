9b03f29b0abe363ab2bd32cf4f14775eb2aba538
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-17 23:12:36 +0000

    retrying to concatenate 2 strings- file 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * str_concat - concatenate 2 strings
  * @s1: string 1
@@ -8,27 +9,28 @@
 **/
 char *str_concat(char *s1, char *s2)
 {
+	int l1 = strlen(s1);
+	int l2 = strlen(s2);
 	int i = 0;
-	int length = 0;
 	char *s3;
 
 	if (s1 == NULL && s2 == NULL)
 {
 		return (" ");
 }
-	s3 = (char *)malloc(sizeof(s1) + sizeof(s2) + 1);
+	s3 = (char *)malloc(sizeof(l1) + sizeof(l2) + 1);
 
 	while (s1[i] != '\0')
 {
-		s3[length] = s1[i];
+		s3[l1] = s1[i];
 		i++;
-		length++;
+		l1++;
 }
 	while (s2[i] != '\0')
 {
-		s3[length] = s2[i];
+		s3[l2] = s2[i];
 		i++;
-		length++;
+		l2++;
 }
 	return (s3);
 }
