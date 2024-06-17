6061c53a1f6c2173fccc342630288c7ce918c1d7
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-17 00:10:28 +0000

    use argv and argc to read file name- changed to a  MACRO function--betty

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main - file name
+ * main - void
  * Returns: file name
 **/
-int  main()
+int  main(void)
 {
 	printf("%s\n", __FILE__);
 		return (0);
