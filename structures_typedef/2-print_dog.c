e760598f75c001b7449fea0a377ec33fa407498e
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-18 22:40:26 +0000

    file 2 started, dog header updated

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - print struct
+ * @d: dog
+ * Return: string
+**/
+void print_dog(struct dog *d)
+{
+	if ()
+{
+		printf("Name: (nil));
+}
+	else (d == NULL)
+{
+		printf();
+}
