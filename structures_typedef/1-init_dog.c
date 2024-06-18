7a6523a91bb6605b85651cbb45ac60798ed7510f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-18 22:29:53 +0000

    file 1, incomplete, will push to save and not create dup

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * init_dog - initialize dog struct
+ *@d: dog
+ *@name: name of dog
+ *@age: dog age
+ *@owner: owner of dog
+ *Return: dog
+**/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*char).name = "Poppy";
+	age = "3.5";
+	dog.owner = "Bob";
+}
