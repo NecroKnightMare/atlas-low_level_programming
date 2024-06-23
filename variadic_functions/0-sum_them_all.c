6931dfedece43e1139759c2da130ce14fd8537f9
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 23:06:57 +0000

    file 0 added int to n in for loop- compiles

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
  * sum_them_all - sum of all parameters
@@ -8,5 +9,20 @@
 **/
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list li;
+	int i, sum;
 
+	if (n == 0)
+{
+		return (0);
+}
+	va_start (li, n);
+
+	sum = 0;
+	for (i = 0; i < (int)n; i++)
+{
+		sum += va_arg (li, int);
+}
+	va_end (li);
+	return (sum);
 }
