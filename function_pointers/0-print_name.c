c3f2e4c65d91ccc4d4cde01823bf3076be8c4c32
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 20:51:08 +0000

    print name in a function

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stddef.h>
 /**
  * print_name - write function that prints name
  * @name: name of function
@@ -9,5 +10,8 @@
 **/
 void print_name(char *name, void (*f)(char *))
 {
-
+	if (name != NULL && f != NULL)
+{
+		printf("%p", name);
+}
 }
