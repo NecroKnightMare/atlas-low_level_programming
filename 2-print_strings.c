6cd24c2e0615f86f23dbef41e5dc4f8352816b61
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 22:25:22 +0000

    file 2 with header

---
+++ 2-print_strings.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - print strings followed by new line
+ * @seperator: string printed between strings
+ * @n: number of strings
+ * Return: void
+**/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+}
