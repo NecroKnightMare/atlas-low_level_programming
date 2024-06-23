6a0a6babd65377b179d5d6e3a5cb99836a3ca9eb
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 21:51:18 +0000

    file 2, same frame as file 1 returning array that has been compared

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -9,5 +9,19 @@
 **/
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+{
+		return (-1);
+}
+	if (array != NULL && size < 0 && cmp != NULL)
+{
+		for (i = 0; i < (int)size; i++)
+{
+			cmp(array[i]);
+}
+		return (i);
+}
+	return (-1);
 }
