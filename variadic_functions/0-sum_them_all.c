9dd97f3a8f6a47a93f9a271b86ac5fb71a11cdca
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 22:34:47 +0000

    moved files, wrong directory

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of all parameters
+ * @n: variable
+ * @...: keeps going
+ * Return: 0 if n is 0
+**/
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
