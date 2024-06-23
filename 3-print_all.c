09acd8b0ad0c153c4848d7e65780994d4a42fb5b
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 22:28:24 +0000

    file 3 with header

---
+++ 3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @format: c-char, i-int, f-float, s-char*
+ * description: only 2 while loops and 2 if statements, max of 9 int, new line
+ * Return: void
+**/
+void print_all(const char * const format, ...)
+{
+
+}
