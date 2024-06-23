4d8c71bdae563051f281a0f1382efdad208f812d
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 23:27:19 +0000

    file 1 - print numbers and strings inbetween numbers, added stdarg lib and others

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,5 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * print_numbers - print numb followed by new line
  * @seperator: string printed between numbers
