7f798d677fc1bb4475652f57668032e16cbfd083
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 18:57:11 +0000

    figure out how to print a float if it isn't what it is suppose to be, just set it to 0

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,6 +1,7 @@
-#include "main.h"
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 /**
  * print_dog - print struct
  * @d: dog
@@ -8,11 +9,25 @@
 **/
 void print_dog(struct dog *d)
 {
-	if ()
+	if (d == NULL)
 {
-		printf("Name: (nil));
 }
-	else (d == NULL)
+	if (d->name == NULL)
 {
-		printf();
+		printf("Name: (nil)\n");
+}
+	if (d->age == 0)
+{
+		printf("Name: (nil)\n");
+}
+	if (d->owner == NULL)
+{
+		printf("Name: (nil)\n");
+}
+	if (d != NULL)
+{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+}
 }
