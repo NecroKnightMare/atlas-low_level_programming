fb49086c733dc7619ccc23946f3f7b38039e32eb
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 19:50:11 +0000

    created file 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - write function that prints name
+ * @name: name of function
+ * @f: ...
+ * @*: ...
+ * Return: void
+**/
+void print_name(char *name, void (*f)(char *))
+{
+
+}
