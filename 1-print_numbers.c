8f75b157a2dc904c878eea259043c06ea87fc40b
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 22:23:24 +0000

    file 1 with header

---
+++ 1-print_numbers.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print numb followed by new line
+ * @seperator: string printed between numbers
+ * @n: number of integers
+ * Return: void
+**/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+}
