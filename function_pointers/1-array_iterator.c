03da83f88d50a02a28481b2dca855212e89c4dab
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 21:30:26 +0000

    go through array and size of array with pointer of function - description too long, works - betty

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * array_iterator - Write a function that executes a function given as a parameter on each element of an array.
+ * array_iterator - Write a function that executes a funct through an array.
  * @array:
  * @size: size of array
  * @action:
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if ( array != NULL && size > 0  && action != NULL)
+	if (array != NULL && size > 0  && action != NULL)
 {
 		for (i = 0; i < (int)size; i++)
 {
