ecff0a37dd21578628cce1b70f4dbd47acc3be96
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-23 21:28:47 +0000

    go through array and size of array with pointer of function

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -9,5 +9,13 @@
 **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	int i;
 
+	if ( array != NULL && size > 0  && action != NULL)
+{
+		for (i = 0; i < (int)size; i++)
+{
+			action(array[i]);
+}
+}
 }
