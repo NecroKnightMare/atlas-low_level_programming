48f72cc7210fd561bd0c2a5c5eac5d4774c09d17
Ariel.Lopez <alexandrea.ariel@atlasschool.com>
2024-06-24 00:29:29 +0000

    file 2 - used same structure as file 1, changed syntax for readability, added parameters for this file--betty

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,11 +1,36 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 /**
  * print_strings - print strings followed by new line
- * @seperator: string printed between strings
+ * @separator: string printed between strings
  * @n: number of strings
  * Return: void
 **/
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+        va_list str;
+        int i;
+        char *string;
 
+        va_start(str, n);
+        for (i = 0; i < (int)n; i++)
+{
+                string = va_arg(str, char *);
+		if (string == NULL)
+{
+		printf("(nil)");
+}
+			else
+{
+			printf("%s", string);
+}
+                if (separator != NULL && i != ((int)n - 1))
+{
+                        printf("%s", separator);
+}
+}
+	printf("\n");
+        va_end(str);
 }
