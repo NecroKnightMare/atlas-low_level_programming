e48a5db597445467a1600882ffa9484542deea7e
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-06-24 21:00:19 +0000

    set up 2/4 files in 3, most of if not all of op _functions is done

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,6 +1,29 @@
-
+#include "calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+**/
 int op_mod(int a, int b)
+{
+	return (a % b);
+}
 int op_div(int a, int b)
+{
+	return (a / b);
+}
 int op_mul(int a, int b)
+{
+	return (a * b);
+}
 int op_sub(int a, int b)
+{
+	return (a - b);
+}
 int op_add(int a, int b)
+{
+	return (a + b);
+}
