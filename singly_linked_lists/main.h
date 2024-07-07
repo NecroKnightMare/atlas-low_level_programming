3f1ba728b656eec3663e68889171e527ffebdc16
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-07 17:18:44 +0000

    added readme, and adjusted main with the addition of putchar, deleted custom linked list and copied instructed list

--- singly_linked_lists/main.h
+++ singly_linked_lists/main.h
@@ -2,20 +2,22 @@
 #define MAIN_H
 #include <stdio.h.>
 #include <stdlib.h>
-#include <
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 
-struct Link {
-int data;
-struct Link *next;
-};
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
-node create(){
-node temp;
-temp = (node)malloc(sizeof(struct Link));
-temp->next = NULL
-return(temp);
-};
+int _putchar(char c);
+{
+	return (write(1, &c, 1));
+}
+
+#endif
