af4a42c4c55d596d4b1e4e48dc26514eb85a3287
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-07 21:27:14 +0000

    file 3, only printed [4] Anne, will push for now to save commit

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,15 +10,36 @@
 **/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	struct Node* new = (struct Node*)malloc(sizeof(struct Node));
-	new->len = added;
-	struct Node* tail = *head;
-	new->next = NULL;
+	list_t *s;/**end node**/
+	list_t *tmp = *head;/**temporary node**/
+	unsigned int len;
 	
+	for (len = 0; str[len]; len++)
+{
+}
+	s = malloc(sizeof(list_t));/**allocate memory at end**/
+
+	if (!s)/**if not new node**/
+{
+		return (NULL);
+}
+
+	s->str = strdup(str);/**dup string into node**/
+	s->len = len;/**make sure its the same size**/
+	s->next = NULL;/**instead of beginning, NULL = tail**/
+
 	if (*head == NULL)
 {
-		*head = new;
+	*head = s;/**head pointer will equal to end node**/
+	return (s);
 }
-	return (*head);
+
+	while (tmp->next)/**tmp will be next node to create space for string**/
+{
+		tmp = tmp->next;/**tmp is next node**/
+
+		tmp->next = s;/**next node is end node**/
+}
+	return(s);
 }
 
