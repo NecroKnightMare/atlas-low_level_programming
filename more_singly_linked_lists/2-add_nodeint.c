c821468428b089d9cbb46788c6e1ba71cea599ac
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-07 23:14:30 +0000

    file 2 rearranged and modified to prototype

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -10,23 +10,17 @@
 **/
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-		list_t *new; /*pointer to string*/
-	unsigned int len = 0; /* size of sll*/
+	listint_t *new; /*pointer to string*/
 
-	while (n[len])
-{
-		len++;
 
-	new = malloc(sizeof(list_t)); /** pointer  will need the size of node**/
-		new->n = strdup(n);/**dup string into new node called s**/
-		new->len = len;/**pointer length equals incremented string**/
-		new->next = *head;/**next node equals head pointer of sll**/
-		*head = new;/**pointer to head equals string**/
-        if (!new)/**if not s free space and ret null**/
+	new = malloc(sizeof(listint_t)); /** pointer  will need the size of node**/
+
+	if (!new)/**if not s free space and ret null**/
 {
-                free(new);
-                return (NULL);
+		return (NULL);
 }
-}
-	return (*head); /**return string thats dup into node head**/
+	new->n = n;/**pointer new is now new node**/
+	new->next = *head;/**next node equals head pointer of sll**/
+	*head = new;/**pointer to head equals new node**/
+	return (*head); /**return string into node head**/
 }
