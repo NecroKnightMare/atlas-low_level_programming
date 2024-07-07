785423417d19d2f9df5f56b4bcff401b73fd2312
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-07 18:17:32 +0000

    deleted code for _putchar, added prototype

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -22,11 +22,7 @@ typedef struct list_s
  *c: character
  *Return: character
 **/ 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
