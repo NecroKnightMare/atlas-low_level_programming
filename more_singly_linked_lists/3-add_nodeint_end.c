a134f65c506e7871c01e1fa1368b23adb93dc782
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-07 23:17:35 +0000

    file 2 still checking, file 3 copien=d from last project, will modify next, taking dinner

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,4 +10,35 @@
 **/
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	list_t *s;/**end node**/
+	list_t *tmp = *head;/**temporary node**/
+	unsigned int len;
+	
+	for (len = 0; str[len]; len++)
+{
+}
+	s = malloc(sizeof(list_t));/**allocate memory at end**/
 
+	if (!s)/**if not new node**/
+{
+		return (NULL);
+}
+
+	s->str = strdup(str);/**dup string into node**/
+	s->len = len;/**make sure its the same size**/
+	s->next = NULL;/**instead of beginning, NULL = tail**/
+
+	if (*head == NULL)
+{
+	*head = s;/**head pointer will equal to end node**/
+	return (s);
+}
+
+	while (tmp->next)/**tmp will be next node to create space for string**/
+{
+		tmp = tmp->next;/**tmp is next node**/
+
+		tmp->next = s;/**next node is end node**/
+}
+	return(s);
+}
