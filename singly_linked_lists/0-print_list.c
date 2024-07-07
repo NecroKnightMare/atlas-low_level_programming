fedc4041bf548613b4a61cb4d9449475e829b618
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-07 18:07:06 +0000

    had to put struct sll in front of prototypes, added unistd lib for putchar

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,23 +3,18 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
- * print_list - prints all elements of list_t
+ *print_list - prints all elements of list_t
  *h: pointer to node in ssl
  *Return: the number of nodes and nil [0] if Null
 **/
 size_t print_list(const list_t *h)
 {
-	char *str = 0;
 	size_t num;
 
-	if (str == NULL)
-{
-		printf("[0] (nil)");
-}
 	for (num = 0; h; num++)
 {
-			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 }
 	return (num);
 }
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,25 +1,37 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stddef.h>
-
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-int list_t *add_node(list_t **head, const char *str);
-int list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
+/**
+ * struct list_s - ssl
+ * @str: malloc'ed string
+ * @len: length of string
+ * @next: next node
+ * Description: a structure of a singly linked list
+**/
 typedef struct list_s
 {
     char *str;
     unsigned int len;
     struct list_s *next;
 } list_t;
-
-int _putchar(char c);
+/**
+ *_putchar - print character
+ *c: character
+ *Return: character
+**/ 
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+int _putchar(char c);
 
 #endif
