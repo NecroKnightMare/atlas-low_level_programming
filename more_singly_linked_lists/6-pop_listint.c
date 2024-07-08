f7d353690417aaa33c137eb9f7ccdf66d625c614
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-08 01:10:28 +0000

    file 6 compiles after inputting pointer in braces, confusing bit

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -10,3 +10,16 @@
 **/
 int pop_listint(listint_t **head)
 {
+	int n;
+	listint_t *ret;/**ret head data and delete head node**/
+
+	if (*head == NULL)
+{
+		ret = (*head)->next;
+		n = (*head)->n;
+		*head = ret;
+
+		free(*head);
+}
+	return(n);
+}
