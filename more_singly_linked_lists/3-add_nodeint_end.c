e5ba8b487a47785a98c9cbad2b26bcb0dc814d9b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-08 00:36:06 +0000

    file 3 compiles, will test and push

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,26 +13,26 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *end;/**end node**/
 	listint_t *tmp = *head;/**temporary node**/
 	
-	end = malloc(sizeof(listint_t));/**allocate memory at end**/
+	tmp = *head;
 
-	if (!end)/**if not new node**/
+	while (tmp && tmp->next != NULL)/**if not new node**/
 {
-		return (NULL);
+		tmp = tmp->next;
+}
+	end = malloc(sizeof(listint_t));/**allocate memory at end**/
+	
+	if (tmp == NULL)
+{
+	return (NULL);
 }
 	end->n = n;/**make sure its the same size**/
-	end->next = NULL;/**instead of beginning, NULL = tail**/
-
-	if (*head == NULL)
+        end->next = NULL;/**instead of beginning, NULL = tail**/
+	
+	if (tmp)
 {
-	*head = end;/**head pointer will equal to end node**/
+	tmp->next = end;/**head pointer will equal to end node**/
+}
+	else
+		*head = end;
 	return (end);
-}
-
-	while (tmp->next)/**tmp will be next node to create space for string**/
-{
-		tmp = tmp->next;/**tmp is next node**/
-
-		tmp->next = end;/**next node is end node**/
-}
-	return(end);
 }
