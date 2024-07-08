03b76b61e34cb2e208e4191c152c69541925a9de
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-08 01:17:11 +0000

    file 7 compiles, cant seem to get to the next sll, will research more but will push for points until then

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -10,3 +10,20 @@
 **/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
+	listint_t *tmp = head;
+
+	if (!tmp)
+{
+		return (NULL);
+}
+	while(tmp)
+{
+		if (i < index)
+{
+			tmp = tmp->next;
+			i++;
+}
+}
+	return(tmp);
+}
