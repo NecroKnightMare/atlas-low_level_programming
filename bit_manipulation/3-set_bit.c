dc98cf2d19310b3ee25567ce67ac2665c0cab62f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-08 23:38:04 +0000

    file 3, kept file 1 and 2 logic, file 3 sets value of *n to 1 at a given index

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
+ *set_bit - set bit to 1
+ *@n: value
+ *@index: given index
+ *Returns: 1 if it worked, or -1 if an error occurred
 **/
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int size;
+	unsigned int m;/**seen mask syntax in research will use m for short**/
+
+	size = (sizeof(n) * 8 - 1);/**copied again from file 1**/
+
+	if (index > size)/**same parameters as long as index is more than size**/
+{
+		return (-1);
+}
+	m = 1 << index;/**sets mask to one at the given index**/
+
+	*n = *n| m;/**new value of pointer**/
+
+	return (1);
+}
