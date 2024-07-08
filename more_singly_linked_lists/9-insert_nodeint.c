10010a1f58011662b811b8c3d5ac4b151c4f811a
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-08 01:44:27 +0000

    file 9 compiles, spelling and a lot of syntax errors, function inserts new node after deleted node

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <string.h>
+#include <stddef.h>
 #include <stdio.h>
 #include "lists.h"
 /**
@@ -10,3 +10,44 @@
 **/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int count = 1;
+	listint_t *tmp;
+	listint_t *in;
+
+	if (head == NULL)
+{
+		return (NULL);
+}
+	tmp = *head;
+
+	while (tmp && count < idx)
+{
+		tmp = tmp->next;
+		count++;
+}
+	in = malloc(sizeof(listint_t));
+
+	if (in == NULL)
+{
+		free(in);
+		return (NULL);
+}
+	in->n = n;
+
+	if (count == 0)
+{
+	*head = in;
+	in->next = tmp;
+}
+		else if (tmp->next)
+{
+			in->next = tmp->next;
+			tmp->next = in;
+}
+		else
+{
+			in->next = NULL;
+			tmp->next = in;
+}
+	return (in);
+}
