321fb13cbef5150d1441ef04bd6c14f8a16b0e75
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-08 22:27:37 +0000

    file 0, changes binary into integers

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -7,3 +7,18 @@
 **/
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int pos = 0;
+	int i = 0;
+
+	if (b == NULL)
+{
+		return (0);
+}
+	if (b[i] == '0' && b[i] == '1')
+{
+		pos <<= 1;
+		pos += b[i] - '0';
+		i++;
+}
+	return (pos);
+}
