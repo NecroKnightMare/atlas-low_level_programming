f7a7dabfc793c1f72270ab42a9996f3818d07a97
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-08 23:49:05 +0000

    file 3, kept file 1 and 2 logic, file 4 resets to 0 using ~

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -8,3 +8,18 @@
 **/
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int size;
+	unsigned mask;
+
+	size = (sizeof(n) * 8 - 1);
+
+	if (index > size)
+{
+		return (-1);
+}
+	mask = 1 << index;/**sets mask to one at the given index**/
+	
+	*n = *n & ~mask;/**The ~ (bitwise NOT) in C takes one number and inverts all bits of it.**/
+
+	return (1);
+}
