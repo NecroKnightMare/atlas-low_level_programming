4850b349f55344e804500fb8c8d9f93763fc8f1b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-08 22:59:08 +0000

    file 0 , halfmarks need something else, file 1, need to compile, print integers as binary

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -8,3 +8,26 @@
 **/
 void print_binary(unsigned long int n)
 {
+	unsigned long int size;
+	int i;
+	int b;
+
+	size = (sizeof(n) * 8 - 1);
+
+	if (n == 0)
+{
+		_putchar('0');
+}
+	if (n == 1)
+{
+		_putchar('1');
+}
+	while (size >= 0)
+{
+		b = ((n >> size) & 1);
+		if (b == '1')
+{
+			_putchar(b + '0');
+}
+		size -= 1;
+}
