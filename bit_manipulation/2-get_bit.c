c48264491be95c6c2717776cc27038a05ec6215f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-08 23:24:46 +0000

    used size from file 1, file 2 compiles and gives bit value for a given index

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
+ *get_bit - gets the bit value of an index
+ *@n: is a value of a positive number
+ *@index: a given number
+ *Returns: the value of the bit at index index or -1 if an error occured
 **/
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int size;
+
+	size = (sizeof(n) * 8 - 1);
+
+	if (index > size)
+{
+		return (-1);
+}
+	return ((n >> index) & 1);
+}
