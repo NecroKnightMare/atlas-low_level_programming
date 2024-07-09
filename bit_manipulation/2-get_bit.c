76e97edd3094218d86e9a643cf892aff6cbef9a0
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 02:40:29 +0000

    file 2 documentation

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * get_bit - gets the bit value of an index
+ *get_bit - gets the bit value of an index
  *@n: is a value of a positive number
  *@index: a given number
  *
- *Returns: the value of the bit at index index or -1 if an error occured
+ *Return: the value of the bit at index index or -1 if an error occured
 **/
 int get_bit(unsigned long int n, unsigned int index)
 {
