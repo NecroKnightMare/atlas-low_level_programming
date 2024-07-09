44a94d7238bfc39f18582a35a33ce7e7a48cd1cd
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 04:09:07 +0000

    file 5, while not null go through and free, set head to Null

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -3,26 +3,19 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
- *
- *
+ *free_listint2 - free sec list
+ *@head: pointer to first node in SSL
+ *Return: nothing void
 **/
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 	
-	if (*head == NULL)
+	while (*head != NULL)
 {
-	return;
-}
-
-	while (*head)
-{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
 }
 	*head = NULL;
 }
