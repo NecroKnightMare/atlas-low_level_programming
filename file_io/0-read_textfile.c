49f0f8dca6f0426bf67aace1dfb2229ef676f32c
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 22:11:38 +0000

    had to replug in my code since none of it saved, ..., used if statements to handle will see if Ii can make smaller in future

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,21 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "main.h"
 /**
- *
- *
- *
+ *read_textfile - read file, print contents, close file
+ *@filename: filepointer
+ *@letters: contents of file
+ *Return: contents of file
 **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
-	ssize_t rd, wrt;
+	int rd, wrt;
 	char *count;
 
-	if (!filename || filename == NULL)
+	if (letters == 0 || filename == NULL)
 {
 		return (0);
 }
-	file = fopen(filename, "r");
-	
-	
+	count = malloc(sizeof(char) * letters);
+
+	if (count == NULL)
+{
+		return (0);
+}
+	file = open("filename", O_RDONLY);
+
+	if (file == -1)
+{
+		free(count);
+		return (0);
+}		
+	rd = read(file, count, letters);
+
+	if (rd == -1)
+{
+		free(count);
+		close(file);
+		return (0);
+}
+	wrt = write(STDOUT_FILENO, count, rd);
+
+	free(count);
+	close(file);
+
+	if (wrt != rd)
+{
+		return (0);
+}
+	return (count);
+}
