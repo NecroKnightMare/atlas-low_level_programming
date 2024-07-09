4813d0a36c433652e3687e619bd0ee07b0722847
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 00:02:14 +0000

    file 3, kept file size, file 5, flip bits from one number to another and ret num

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -8,4 +8,21 @@
 **/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int bits = 0;
+	unsigned long int i = 0;
+	unsigned long size;
 
+	size = (sizeof(n) * 8 - 1);/**copied again from file 1**/
+
+	while (i <= size)
+{
+		if ((!n == 0) && (!m == 0))
+{
+			bits++;
+}
+		n = n >> 1;
+		m = m >> 1;
+		i++;;
+}
+	return (bits);
+}
