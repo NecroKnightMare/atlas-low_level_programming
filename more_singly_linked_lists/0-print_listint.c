b148a46c824b85696b8d5f614576fa4203dc6b23
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 02:55:55 +0000

    file 0 documentation

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -3,18 +3,18 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ *print_listint - print list
+ *@h: pointer to value
  *
- *
- *
- *
+ *Return: number of integers
 **/
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
-	
+
 	while (h)
 {
-	if (!h)		
+	if (!h)
 {
 		printf("%d\n", h->n);
 		h = h->next;
