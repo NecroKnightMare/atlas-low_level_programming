d14d83752a2cf147c599be34c40a11c7242fa3bf
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 02:20:03 +0000

    file 0 compiles, missing something, converts bin to int, readded NULL handle

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -14,22 +14,11 @@ unsigned int binary_to_uint(const char *b)
 {
 		return (0);
 }
-	while (b[i] != '\0')
+	if (b[i] != '0' && b[i] != '1')
 {
+		pos <<= 1;
+		pos += b[i] - '0';
 		i++;
-		if (b[i] == '1')
-{
-		pos = (pos << 1) | 1;
-}
-			else if (b[i] == '0')
-{
-				pos <<= 1;
-}
-			else if (b[i] != '0' && b[i] != '1')
-{
-				i++;
-				return (0);
-}
 }
 	return (pos);
 }
