63064871e6b6d3a5d2b9da54c3577bf7e96c8559
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 20:53:12 +0000

    file 10 added documentation

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
+ *delete_nodeint_at_index - delete node at given index
+ *@head: head of index
+ *@index: index given
+ *Return: 1 if success, -1 if failure
 **/
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-
+}
