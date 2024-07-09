e5f7ef25a1525a15b0451a04bb81e74998b1c1fa
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 04:14:18 +0000

    file 6 reconfigured, delete node and ret head data

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -3,23 +3,25 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
- *
+ *pop_listint - delete node and ret head data
+ *@head: head node in SLL
+ *Return: head data
 **/
 int pop_listint(listint_t **head)
 {
 	int n;
+
 	listint_t *ret;/**ret head data and delete head node**/
 
 	if (*head == NULL)
 {
-		ret = (*head)->next;
-		n = (*head)->n;
-		*head = ret;
-
-		free(*head);
+	return (0);
 }
+	ret = *head;
+	n = (*head)->n;
+	*head = (*head)->next;
+
+	free(ret);
+
 	return(n);
 }
