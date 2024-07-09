3b451a1592e59b6bf5caa848584f959b51440336
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 02:29:33 +0000

    file 0 broken down to many lines instead of piping most

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,25 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int pos = 0;
 	unsigned int i = 0;
 
-	if (b == NULL)
+/**	if (b == NULL)
 {
 		return (0);
-}
-	while (b[i] == '0' && b[i] == '1')
+}**/
+	if (b[i] == '1')
 {
+		pos = (pos << 1) | 1;
 		i++;
 		return (1);
-	if (b[i] != '0' && b[i] != '1')
+}
+	if (b[i] == '0')
 {
 		pos <<= 1;
-		pos += b[i] - '0';
 		i++;
 }
+		else if (b[i] != '0' && b[i] != '1')
+{
+			i++;
+			return (0);
 }
 	return (pos);
 }
