e1064e3fc23520c34a766bac5ace75ed6b07ac59
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 02:30:42 +0000

    file 0 broken down to many lines instead of piping most, decommented NULL

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,10 +10,10 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int pos = 0;
 	unsigned int i = 0;
 
-/**	if (b == NULL)
+	if (b == NULL)
 {
 		return (0);
-}**/
+}
 	if (b[i] == '1')
 {
 		pos = (pos << 1) | 1;
