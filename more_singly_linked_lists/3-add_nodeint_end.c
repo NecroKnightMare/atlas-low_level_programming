5eb9f6c5230f86e3cfb6d3c3294c846a943d2cb4
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 03:19:07 +0000

    file 3 added if statements and rearranged node movement

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,36 +3,39 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- *
- *
- *
- *
+ *add_nodeint_end - add node to end
+ *@head: pointer to head of sll
+ *@n: value
+ *Return: end node
 **/
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *end;/**end node**/
-	listint_t *tmp = *head;/**temporary node**/
-	
-	tmp = *head;
 
-	while (tmp != NULL)/**if not new node**/
+	listint_t *tmp;/**temporary node**/
+
+	end = malloc(sizeof(listint_t));/**allocate memory at end**/
+
+	if (end == NULL)
+{
+		return (NULL);
+}
+	if (tmp != NULL)/**if not new node**/
+{
+		tmp = *head;
+
+		while (tmp->next != NULL)
 {
 		tmp = tmp->next;
 }
-	end = malloc(sizeof(listint_t));/**allocate memory at end**/
-	
-	if (end == NULL)
-{
-	return (NULL);
-}
-	end->n = n;/**make sure its the same size**/
-        end->next = NULL;/**instead of beginning, NULL = tail**/
-	
-	if (tmp)
-{
 	tmp->next = end;/**head pointer will equal to end node**/
 }
 	else
+{
 		*head = end;
+}
+	end->next = NULL;
+	end->n = n;
+
 	return (end);
 }
