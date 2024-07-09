7b2bcfb0526df4758ae4a506bd6e3141642cfc07
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 03:10:26 +0000

    file 2 betty

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -11,9 +11,9 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new; /*pointer to string*/
-	
+
 	new = *head;
-	
+
 	new = malloc(sizeof(listint_t)); /** pointer  will need the size of node**/
 
 	if (new == NULL)/**if not s free space and ret null**/
