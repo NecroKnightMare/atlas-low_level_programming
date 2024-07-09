23bb83752fade1cc031a047c7e7a7831f3bd6b4c
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 03:06:16 +0000

    file 2 initialized new for head in beginning

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
- *
+ *add_nodeint - add node 
+ *@head: pointer to head of sll
+ *@n: value
+ *Return: Null if null, new node if success
 **/
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new; /*pointer to string*/
-
+	new = *head
 	new = malloc(sizeof(listint_t)); /** pointer  will need the size of node**/
 
 	if (new == NULL)/**if not s free space and ret null**/
