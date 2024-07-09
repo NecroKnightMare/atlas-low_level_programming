b0776fb0334855ebd6b76c7ab26d452e0ef75370
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 23:44:37 +0000

    beginning structure -pseudocode- of most parameters

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,3 +1,7 @@
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
@@ -8,3 +12,24 @@
  *
 **/
 
+	char buffer [1024];
+
+	if (file_to)
+{
+		O_TRUNC
+}
+	if (!file_from || !read(file_from))
+{
+		dprintf("Error: Can't read from the file NAME_OF_THE_FILE\n");
+		exit(98);
+}
+	if (!creat(file_to) || write(file_to))
+{
+		dprintf("Error: Can't read from the file NAME_OF_THE_FILE\n");
+		exit(99);
+}
+	if (!close(file_to) || !close(file_from))
+{
+		 dprintf("Error: Can't close fd FD_VALUE\n");
+                exit(100);
+}
