73abda27b88095b1013d817ec7685b85a8e51ab0
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 02:00:32 +0000

    file 0 made adjustments need to compile

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,24 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int pos = 0;
-	int i = 0;
+	unsigned int i = 0;
 
 	if (b == NULL)
 {
 		return (0);
 }
-	if (b[i] == '0' && b[i] == '1')
+	while (b[i] != '\0')
 {
-		pos <<= 1;
+		pos = pos << 1;
+		if (b[i] != '0' && b[i] != '1')
+{
+		return (0);
+}
+		else if (b[i] == '1')
+{
 		pos += b[i] - '0';
 		i++;
+}
 }
 	return (pos);
 }
