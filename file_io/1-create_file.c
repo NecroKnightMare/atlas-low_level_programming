fb57bdf1e528095a0175d9d7fccb913849e46a71
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 22:48:41 +0000

    been awhile using file permissions, chmod didn't work for file 0, piping to avoid long lines of code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,11 +1,39 @@
-#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
- *
+ *create_file - create file
+ *@filename: file name
+ *@text_content: text in file
+ *Return: 1 if success; -1 on failure
 **/
 int create_file(const char *filename, char *text_content)
 {
+	int file;
+	int length = 0;
+	int count = 0;
+
+	if (!filename || filename == NULL)
+{
+		return (-1);
+}
+	if (text_content == NULL)
+{
+		file = open(filename, O_CREAT |  O_WRONLY | O_TRUNC , 600);
+}
+	if (text_content != NULL)
+{
+		text_content++;
+		count++;
+		length = write(file, text_content, count);
+}
+	if (close(file) == -1 || count != length)
+{
+		return (-1);
+}
+return (1);
+}
