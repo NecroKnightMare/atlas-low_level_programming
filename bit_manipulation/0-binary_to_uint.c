53a5ccd3c57248c03170784dab69bbdb7b7fb304
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-09 02:08:38 +0000

    file 0 compiles, rehandled errors and paramaters, converts bin to int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,20 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int pos = 0;
 	unsigned int i = 0;
 
-	if (b == NULL)
-{
-		return (0);
-}
 	while (b[i] != '\0')
 {
-		pos = pos << 1;
-		if (b[i] != '0' && b[i] != '1')
-{
-		return (0);
-}
-		else if (b[i] == '1')
-{
-		pos += b[i] - '0';
 		i++;
+		if (b[i] == '1')
+{
+		pos = (pos << 1) | 1;
+}
+			else if (b[i] == '0')
+{
+				pos <<= 1;
+}
+			else if (b[i] != '0' && b[i] != '1')
+{
+				return (0);
 }
 }
 	return (pos);
