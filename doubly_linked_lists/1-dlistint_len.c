17d7b1af3b46ba498268183f8ff0bc7a3d2847c6
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-10 22:22:34 +0000

    file 1, print node length of doubly linked list, tested and compiled, test runs larger number, will see if linter wants that parameter

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *dlistint_len - returns length of a DLL
+ *@h: start of LL
+ *Return: number of elements in LL
+**/
+size_t dlistint_len(const dlistint_t *h)/**print length of LL**/
+{
+	size_t length;/**use length for readability**/
+
+	while (h)/**while DLL--**/
+{
+		length++;/**increment length**/
+		h = h->next;/**go through nodes**/
+}
+	return (length);/**return length of LL**/
+}
