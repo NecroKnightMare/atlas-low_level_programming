22acb8e6ebfd07fea0d83045077d2e6db19fb838
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-10 22:29:31 +0000

    file 1, print node length of doubly linked list, tested and compiled, forgot to declare length as 0

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -9,7 +9,7 @@
 **/
 size_t dlistint_len(const dlistint_t *h)/**print length of LL**/
 {
-	int length;/**use length for readability**/
+	int length = 0;/**use length for readability**/
 
 	while (h)/**while DLL--**/
 {
