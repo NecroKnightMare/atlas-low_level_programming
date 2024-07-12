ebc2b955487a0c5d25eb041314bfd1799154deee
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-12 00:20:53 +0000

    file 6 structure of sLL will mod to DLL

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *
+ *
+ *
+**/
+int sum_dlistint(dlistint_t *head)
+{
+	int count = 0;
+
+	if (!head)
+{
+	return (0);
+}
+	while (head)
+{
+		count += head->n;
+		head = head->next;
+}
+	return (count);
+}
