968234af10338c6774b69448657bd90bb9f0ff3a
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-13 21:28:14 +0000

    tested and compiled, added function to create node, re3called in function to grab size of new node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ *dlistint_t new_node - make new node
+ *@n: data
+ *Return: new node
+**/
+dlistint_t *NodeN(int data)
+{
+	dlistint_t* new = (dlistint_t *)malloc(sizeof(dlistint_t));
+	
+	new->n = data;
+	new->prev = NULL;
+	new->next = NULL;
+	return (new);
+}
+/**
  *add_dnodeint - add node at the beginning
  *@head: head of DLL
  *@n: data value of DLL
@@ -10,17 +24,14 @@
 **/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;/**new node**/
+	dlistint_t *new = NodeN(n);/**recall for size of struct of node**/
 
-	new = *head;/**new pointer = head pointer**/
-
-	new = malloc(sizeof(dlistint_t));/**grab size of node**/
-
-	if (new == NULL)/**if new fails and = NULL**/
+	if (*head == NULL)/**if new fails and = NULL**/
 {
-		return (NULL);
+		*head = new;/**pointer wil equal new node**/
+		return (NULL);/**if fail**/
 }
-	new->n = n;/**new goes into n(data) and replaces data**/
+	(*head)->prev = new;/**pointer goeas into prev and is the new node**/
 	new->next = *head;/**the new goes into next and = head p**/
 	*head = new;/**pointer = new node**/
 
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -25,5 +25,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+struct dlistint_t new_node(const int n);/**created**/
 
 #endif
