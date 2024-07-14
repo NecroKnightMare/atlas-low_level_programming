5cb836dedd309b27e29897e3daa8eaffb4bdfb7b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-14 03:10:45 +0000

    file 3 adds new node to end and creates if empty to head, tested and compiled

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,26 +10,35 @@
 **/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new;/**new node for end will equal tail**/
+	dlistint_t *new;
+	dlistint_t *end;
 
-	new = malloc(sizeof(dlistint_t));/**grab size of node**/
 
-	new->prev = NULL;
+	new = malloc(sizeof(dlistint_t));
+
+	if (!new)
+{
+		return (NULL);
+}
+
 	new->n = n;
 	new->next = NULL;
 
 	if (*head == NULL)
 {
+		new->prev = NULL;
 		*head = new;
-		return (NULL);
 }
-	dlistint_t *tail = head;
-
-	while (tail->next != NULL)
+	else
 {
-		tail = tail->next;
-		tail->next = new;
-		new->prev = tail;
+	end = *head;
+
+	while (end->next != NULL)
+{
+	end = end->next;
+}	
+	end->next = new;
+	new->prev = end;
 }
-	return (new);
+	return (end);
 }
