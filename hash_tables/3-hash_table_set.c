66f66ee3791276b7643fedd7f6c16df27890e295
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-14 20:27:04 +0000

    file 3 one wrong output, case: hashtableset

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	while (present)
 {
-		if (strdup(present->key) == 0)
+		if (strcmp(present->key, key) == 0)
 {
 			free(present->value);
 			present->value = strdup(value);
