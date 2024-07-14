903181124d79e88913ec62ea9e3f2b9fde670ff8
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-14 04:37:24 +0000

    file 7 betty comments updated

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
- *
+ *insert_dnodeint_at_index - insert new node at index
+ *@h: pointer to DLL
+ *@idx: position in DLL
+ *@n: data value
+ *Return: return node at index
 **/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
