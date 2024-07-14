31d2ee073943f5f8fa074c737185215fb5f1b036
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-14 04:31:25 +0000

    file 7 adds node any where in DLL, base creation of node, not compiled

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,46 +10,19 @@
 **/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 1;
-	listint_t *tmp;
-	listint_t *new;
+	dlistint_t *new;
 
-	idx = 0;
+	new = malloc(sizeof(dlistint_t));
 
-	if (head == NULL)
+	if (!new)
 {
 		return (NULL);
 }
-	tmp = *head;
-
-	while (tmp && count < idx)
-{
-		tmp = tmp->next;
-		count++;
-}
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-{
-		free(new);
-		return (NULL);
-}
+	new->prev = NULL;
 	new->n = n;
+	new->next = NULL;
 
-	if (count == 0)
-{
-	*head = new;
-	new->next = tmp;
-}
-		else if (tmp->next)
-{
-			new->next = tmp->next;
-			tmp->next = new;
-}
-		else
-{
-			new->next = NULL;
-			tmp->next = new;
-}
+
+
 	return (new);
 }
