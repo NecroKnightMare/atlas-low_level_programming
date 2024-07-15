93d3d8eedf9e7036341556c6ee10500d0bd2b70d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-15 19:40:06 +0000

    playing ring around the rosie with this one, another return

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -35,7 +35,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 }
 		if (!temp)
 {
-			return (1);/**changed--out of range handle?**/
+			return (-1);/**changed--out of range handle?--again**/
 }
 		if (temp->prev != NULL)
 {
