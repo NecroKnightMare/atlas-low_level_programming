e54cbaa9d119b04ba9bb4370218ff322a3629a32
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-15 19:10:28 +0000

    says test isn't a directory?, compile, will push to see if it works

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -3,12 +3,43 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
- *
+ *delete_dnodeint_at_index = delete node at index
+ *@head: pointer to DLL
+ *@index: specific node in DLL
+ *Return: 1 if success; -1 if failed
 **/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *temp;
 
+	if (!*head)
+{
+		return (-1);
+}
+	if (index == 0)
+{
+		temp = *head;
+		*head = (*head)->next;
+		if (*head)
+{
+			(*head)->prev = NULL;
+			free(temp);
+			return (-1);
+}
+}
+		temp = *head;
+		if (!temp)
+{
+			return (-1);
+}
+		if (temp->prev != NULL)
+{
+			(*head)->prev->next = (*head)->next;
+}
+		if (temp->next != NULL)
+{
+			(*head)->next->prev = (*head)->prev;
+}
+	free(temp);
+	return (1);
 }
