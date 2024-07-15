ba589388dc98fda88bec1264465972b16bc2717f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-15 20:31:14 +0000

    reverting

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -40,7 +40,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (temp->next != NULL)
 {
 			(*head)->next->prev = (*head)->prev;
-			free(temp);
 			return (1);
 }
 	/**free(head);leaking mem added free- didn't work**/
