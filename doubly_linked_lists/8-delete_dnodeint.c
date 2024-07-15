25af3d5fdf42682edf44e9131196709d51667afa
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-07-15 20:29:40 +0000

    messing with free

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -23,7 +23,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (*head)
 {
 			(*head)->prev = NULL;
-			free(temp);
+			free(head);
 			return (1);/**changecorrect**/
 }
 }
@@ -35,7 +35,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (temp->prev != NULL)
 {
 			(*head)->prev->next = (*head)->next;
-			free(temp);
+			/**free(temp); will see if not needed**/
 			return (1);/**changed to 1**/
 }
 		if (temp->next != NULL)
