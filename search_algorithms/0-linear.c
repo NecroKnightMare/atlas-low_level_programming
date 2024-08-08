8d3325e0caabb52c87a4f641b6da49dd7fe7aa25
Ariel Lopez <alexandrea.lopez@atlasschool.com>
2024-08-08 00:59:45 +0000

    filled in functions and guards in header, filled in doc for files 0 and 1, also pasted function

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ *linear_search - search alg that is a constant
+ *@array: array
+ *@value: value of output
+ *@size: size of array
+ *Return: -1
+*/
+int linear_search(int *array, size_t size, int value)
+{
+
+	return (-1);
+}
---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ *binary_search - algorithm 
+ *@array: array
+ *@size: size of array
+ *@value: value of output
+ *Return: -1
+**/
+int binary_search(int *array, size_t size, int value)
+{
+
+	return (-1);
+}
