e0482072b829b62f2b3a273490c53f2906ecc187
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 17:59:44 +0000

    getting file 1 started

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -8,6 +8,17 @@
 **/
 int binary_search(int *array, size_t size, int value)
 {
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t midd, i;
+
+	if (array == NULL)
+{
+		return (-1);
+}
+	while (left <= right)
+{
+		printf("Searching in array: ");
 
 	return (-1);
 }
