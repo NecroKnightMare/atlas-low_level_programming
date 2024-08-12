774d93a04ec3713f1a37a907166fafc3547e53f6
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-08-12 18:21:51 +0000

    searches through array from left to right then middle until in order or otherwise

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -19,6 +19,30 @@ int binary_search(int *array, size_t size, int value)
 	while (left <= right)
 {
 		printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+{
+		printf("%d", array[i]);
+		if (i < right)
+{
+			printf(", ");
+}
+}
+}
+	print("\n");
 
+	midd = (left + right) / 2;
+
+	if (array[midd] == value)
+{
+	return (midd);
+}
+	else if (array[midd] < value)
+{
+	left = midd + 1;
+}
+	else
+{
+	right = midd -1;
+}
 	return (-1);
 }
